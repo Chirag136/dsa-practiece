267775828c646c80f401ea78cc5534563059bf70
Chirag136 <chirag.p10032006@gmail.com>
2026-08-12 21:10:12 +0530

    Add intersection function for two integer vectors
    Implement intersection method to find common elements between two vectors.

---
+++ array/intersection.cpp
@@ -0,0 +1,27 @@
+class Solution {
+  public:
+    vector<int> intersection(vector<int> &a, vector<int> &b) {
+        // code here
+        vector<int> inter;
+        int n1=a.size();
+        int n2=b.size();
+        int i=0;
+        int j=0;
+        while(i<n1&&j<n2){
+            if(a[i]<b[j]){
+                i++;
+            }
+            else if(a[i]>b[j]){
+                j++;
+            }
+            else{
+                if(inter.size()==0||inter.back()!=a[i]){
+                inter.push_back(a[i]);
+                i++;
+                j++;
+                }
+            }
+        }
+        return inter;
+    }
+};
